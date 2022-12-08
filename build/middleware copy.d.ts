59f1191a0016da7866dfa800a88a30c8c7ae2c14
Anis Marrouchi <anismarrouchi@hotmail.com>
2022-12-08 23:37:23 +0100

    chore: build source files

--- build/middleware copy.d.ts
+++
@@ -1,9 +0,0 @@
-import { IConcept, ITechnology } from 'kodyfire-core';
-import { Concept as BaseConcept } from './concept';
-export declare class Middleware extends BaseConcept {
-    constructor(concept: Partial<IConcept>, technology: ITechnology);
-    generate(_data: any): Promise<void>;
-    getFilename(data: any): any;
-    getExtension(templateName: string, defaultExtension?: string): string | undefined;
-    getTemplatesPath(): string;
-}
